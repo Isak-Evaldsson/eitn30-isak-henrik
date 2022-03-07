dbc6b1d312caf1a1dc1be8b13dcfae3b83c1136d
Isak Evaldsson <isak.evaldsson@outlook.com>
2022-03-07 15:11:51 +0000

    Set correct mtu

--- src/tun.cpp
+++ src/tun.cpp
@@ -47,7 +47,8 @@ int tun_alloc(char *dev)
 }
 
 // Calls appropraite bash commands to setup the tun device correctly
-void setup(std::string addr) {
+void setup(std::string addr)
+{
     char dev[IFNAMSIZ + 1]; // array containg tun device name
     char buf[2048];
 
@@ -55,7 +56,8 @@ void setup(std::string addr) {
 
     // Allocate the tun device
     tun_fd = tun_alloc(dev);
-    if (tun_fd < 0) {
+    if (tun_fd < 0)
+    {
         printf("Could not create tun device");
         exit(0);
     }
@@ -64,14 +66,17 @@ void setup(std::string addr) {
     std::string command = "sudo ip addr add " + addr + " dev tun0";
 
     system("sudo ip link set tun0 up");
+    system("sudo ip link set dev tun0 mtu 900");
     system(command.c_str());
 }
 
-void write_tun(char* packet, int size) {
-    write(tun_fd, packet, size);  
+void write_tun(char *packet, int size)
+{
+    write(tun_fd, packet, size);
 }
 
-ssize_t read_tun(char* buf, size_t s) {
+ssize_t read_tun(char *buf, size_t s)
+{
     return read(tun_fd, buf, s);
 }
 
@@ -87,13 +92,12 @@ uint16_t print_header(char *buf)
     lenght = lenght << 8;
     lenght += buf[3];
 
-    
     int icmp1 = buf[22];
     int icmp2 = buf[23];
-    int icmp_check = (buf[22] << 8)  + buf[23];
+    int icmp_check = (buf[22] << 8) + buf[23];
     int tcp_hdr_len = buf[32] >> 4;
     int icmp_seq = (buf[26] << 8) + buf[27];
-    printf("IP packet, version: %d, ihl: %d, protocol: %d, length: %+" PRIu16"", version, ihl, protocol, lenght);
+    printf("IP packet, version: %d, ihl: %d, protocol: %d, length: %+" PRIu16 "", version, ihl, protocol, lenght);
     printf(" src: %d.%d.%d.%d, dst: %d.%d.%d.%d", buf[12], buf[13], buf[14], buf[15], buf[16], buf[17], buf[18], buf[19]);
     printf(" tcp hdr lenght %d", tcp_hdr_len);
     printf(" icmp seq nbr: %d", icmp_seq);
@@ -102,11 +106,11 @@ uint16_t print_header(char *buf)
     return lenght;
 }
 
-void split_packet(char *buf, uint16_t lenght, std::map<unsigned int, TransmittBuffer>* transmittMap, TransmittBuffer* transBuffer, bool singleBuffer) {
+void split_packet(char *buf, uint16_t lenght, std::map<unsigned int, TransmittBuffer> *transmittMap, TransmittBuffer *transBuffer, bool singleBuffer)
+{
     // only split IPv4 packets
-    if (buf[0] >> 4 != 4) 
+    if (buf[0] >> 4 != 4)
         return;
-
 
     int id = rand() % 512;
     int nbr_full_packets = lenght / 30;
@@ -115,29 +119,35 @@ void split_packet(char *buf, uint16_t lenght, std::map<unsigned int, TransmittBu
     unsigned int dst;
 
     // If last packets is full, modolus will result in 0
-    if(len_last_packet == 0) {
+    if (len_last_packet == 0)
+    {
         nbr_packets = nbr_full_packets;
-        len_last_packet = 30; 
+        len_last_packet = 30;
     }
 
-    if (!singleBuffer) {
-        dst =  (buf[19]<<0) | (buf[18]<<8) | (buf[17]<<16) | (buf[16]<<24);
+    if (!singleBuffer)
+    {
+        dst = (buf[19] << 0) | (buf[18] << 8) | (buf[17] << 16) | (buf[16] << 24);
     }
 
-    for (int i = 0; i < nbr_packets; i++) {
+    for (int i = 0; i < nbr_packets; i++)
+    {
         // sets length depeding if last packet
         int len = i == nbr_packets - 1 ? len_last_packet : 30;
-        
+
         // Copy part of ip-packet
-        char* data = (char*) calloc(len, sizeof(char));
+        char *data = (char *)calloc(len, sizeof(char));
         memcpy(data, buf + (i * 30), sizeof(char) * len);
 
-        DataFrame* item = new DataFrame(data, len, id, i, i == nbr_packets - 1);
+        DataFrame *item = new DataFrame(data, len, id, i, i == nbr_packets - 1);
         //hex_dump(item->data, item->size);
-        
-        if(!singleBuffer) {
+
+        if (!singleBuffer)
+        {
             transmittMap->at(dst).pushDataFrame(item);
-        } else {
+        }
+        else
+        {
             transBuffer->pushDataFrame(item);
         }
     }
@@ -145,21 +155,23 @@ void split_packet(char *buf, uint16_t lenght, std::map<unsigned int, TransmittBu
     printf("Packet spit into %d fragments\n", nbr_packets);
 }
 
-unsigned int get_dest(char *buff) {
-    return (buff[19]<<0) | (buff[18]<<8) | (buff[17]<<16) | (buff[16]<<24);
+unsigned int get_dest(char *buff)
+{
+    return (buff[19] << 0) | (buff[18] << 8) | (buff[17] << 16) | (buff[16] << 24);
 }
 
-void hex_dump(char *buff, uint16_t len) {
+void hex_dump(char *buff, uint16_t len)
+{
     for (size_t i = 0; i < len; i++)
     {
         printf("%02x ", buff[i]);
     }
-    
+
     printf("\n");
-    
 }
 
-int packet_len(char *buf) {
+int packet_len(char *buf)
+{
     int lenght = buf[2];
     lenght = lenght << 8;
     lenght += buf[3];
