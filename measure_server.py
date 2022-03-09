10db2d057bee3ca8052f1b0a21956daa6324a45a
Isak Evaldsson <isak.evaldsson@outlook.com>
2022-03-09 09:14:35 +0000

    Fixed measure_server

--- measure_server.py
+++ measure_server.py
@@ -2,37 +2,33 @@ import socket
 
 host = ''        # Symbolic name meaning all available interfaces
 rec_port = 12345     # Arbitrary non-privileged port
-send_port = 54321
 
-s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# Allows re-connection to a socket in use
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-s1.bind((host, rec_port))
-s2.bind((host, send_port))
+# Socket sends data immidatley
+s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 
-s1.listen(1)
-s2.listen(1)
+s.bind((host, rec_port))
+s.listen(1)
 
-conn1, addr = s1.accept()
-conn2, _ = s2.accept()
+conn, addr = s.accept()
 
 print('Connected by', addr)
 while True:
 
     try:
-        data = conn1.recv(1024)
+        data = conn.recv(6)
 
         if not data: break
         
-        # Echoes back the conntent of the packet to the other socket
-        conn2.sendall(data)
+        # Echoes back the conntent of the packet
+        conn.sendall(data)
 
     except socket.error:
         print("Error Occured.")
         break
 
-conn1.close()
-conn2.close()
+conn.close()
